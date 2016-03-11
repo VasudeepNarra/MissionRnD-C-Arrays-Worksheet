67a0f468aaea6e9ad4b976f920830d610272d433
VasudeepNarra <VasudeepNarra@missionrnd.com>
2016-03-11 23:18:19 +0530

    Submit Commit

--- src/removeArrayDuplicates.cpp
+++ src/removeArrayDuplicates.cpp
@@ -17,6 +17,31 @@ NOTES: Don't create new array, try to change the input array.
 
 int removeArrayDuplicates(int *Arr, int len)
 {
-	
-	return -1;
+	int i, j, k;
+	if (len <= 0 || Arr == NULL) return -1;
+	for (i = 0; i < len; i++)
+	{
+		for (j = i + 1; j < len;)
+		{
+			if (Arr[j] == Arr[i])
+			{
+				for (k = j; k < len; k++)
+				{
+					Arr[k] = Arr[k + 1];
+				}
+				len--;
+			}
+			else
+			{
+				j++;
+			}
+		}
+	}
+	/*for (j = 0; j < len; j++)
+	{
+	printf("%d /t", Arr[j]);
+	}*/
+	return j;
+
+	//return -1;
 }
